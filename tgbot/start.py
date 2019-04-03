3297e2556e22b8572da5d4cdf2677d4f70c427bf
safon18 <45454069+safon18@users.noreply.github.com>
2019-04-03 09:53:13 +0300

    Нашел вторую библиотеку pyrogram, тестирую
    Не получается продвинуться в авторизации, погуглил причины:
    "Регистрация/Login и Logout. При регистрации необходимо учесть разные сценарии: когда код доступа присылается SMS или в другой телеграм клиент, двухфакторную авторизацию и т.п. Самая большая сложность — это тестирование. Любая авторизация более 3-х раз вела к блокировке аккаунта на 24 часа, поэтому тестировать Logout было особенно весело. Несмотря на то, что регистрация нужна всего лишь один раз, наверное это самая сложная часть интеграции." [https://habr.com/ru/post/424245/]

--- tgbot/start.py
+++ tgbot/start.py
@@ -1,19 +1,16 @@
 from telethon import TelegramClient, sync
 import socks
 import socket
+import requests
+import urllib3
 api_id = 563429
 api_hash = '84297edbc5235720e01ddb6583aeca79'
 
-proxy = socks.socksocket()
-proxy.set_proxy(socks.PROXY_TYPE_SOCKS5, addr='76.98.94.54 ', port='35753')
-
-#socks.setdefaultproxy(socks.PROXY_TYPE_HTTP, '94.177.252.200', port=8080)
-#socket.socket = socks.socksocket
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, addr='191.101.148.67', port='65234', username='osafon19', password='G1r0DvR')
+socket.socket = socks.socksocket
 
 print('hello')
 
-client = TelegramClient(session='sesion-name.session', api_id=api_id, api_hash=api_hash, proxy=proxy, timeout=5, request_retries=1, connection_retries=1)
+client = TelegramClient(session='sesion_name.session', api_id=api_id, api_hash=api_hash, timeout=5, request_retries=1, connection_retries=1)
 client.start()
 
-me = client.get_me()
-print(me.stringify())
