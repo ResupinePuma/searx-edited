7129c5c023febd708def023c2a3c01f404ff87ce
safon18 <45454069+safon18@users.noreply.github.com>
2019-04-03 09:57:37 +0300

    Ошибка авторизации

--- tgbot/tgbot2_pyrogram/start.py
+++ tgbot/tgbot2_pyrogram/start.py
@@ -1,3 +1,13 @@
+from pyrogram import Client
+import socks
+import socket
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, addr='191.101.148.67', port='65234', username='osafon19', password='G1r0DvR')
+socket.socket = socks.socksocket
 
+app = Client(
+    "my_account",
+    api_id=563429,
+    api_hash="84297edbc5235720e01ddb6583aeca79"
+)
 
 
